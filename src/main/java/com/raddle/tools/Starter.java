1086c71bccc4675bfbc31c877f860e5791598d51
raddle60 <raddle60@ce383792-5123-98de-8feb-8d02cf8f469b>
2010-06-22 05:09:18 +0000

    生成启动脚本

---
+++ src/main/java/com/raddle/tools/Starter.java
@@ -0,0 +1,132 @@
+package com.raddle.tools;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import javax.swing.JOptionPane;
+
+/**
+ * java 启动程序
+ */
+public class Starter {
+
+	public static void main(String[] args) {
+		try {
+			Properties p = new Properties();
+			File f = new File("starter.properties");
+			if (f.exists()) {
+				p.load(new FileInputStream(f));
+				String mainClass = p.getProperty("main.class");
+				if (mainClass == null || mainClass.trim().length() == 0) {
+					JOptionPane.showMessageDialog(null, "请在starter.properties配置Main Class！");
+					return;
+				}
+				// classpath路径
+				mainClass = mainClass.trim();
+				String pClasspath = p.getProperty("classpath");
+				Set<String> classpathes = new LinkedHashSet<String>();
+				classpathes.add("classes");
+				if (pClasspath != null) {
+					String[] cls = pClasspath.split(",");
+					for (String s : cls) {
+						if (s != null && s.trim().length() > 0) {
+							classpathes.add(s.trim());
+						}
+					}
+				}
+				// lib路径
+				String plibDir = p.getProperty("libDir");
+				Set<String> libDirs = new LinkedHashSet<String>();
+				libDirs.add("lib");
+				if (plibDir != null) {
+					String[] dirs = plibDir.split(",");
+					for (String s : dirs) {
+						if (s != null && s.trim().length() > 0) {
+							classpathes.add(s.trim());
+						}
+					}
+				}
+				// 命令行
+				StringBuffer commandLine = new StringBuffer();
+				String osName = System.getProperties().getProperty("os.name");
+				if (osName.toLowerCase().indexOf("windows") != -1) {
+					// windows
+					commandLine.append("cmd /c \"start javaw -cp ");
+					appendCpDirs(classpathes, libDirs, commandLine, ";");
+					commandLine.append(" ").append(mainClass).append("\"");
+					// 命令行文件
+					Writer w = new FileWriter(new File("start.bat"));
+					w.write(commandLine.toString());
+					w.close();
+				} else {
+					// 其他
+					commandLine.append("java -cp ");
+					appendCpDirs(classpathes, libDirs, commandLine, ":");
+					commandLine.append(" ").append(mainClass);
+					// 命令行文件
+					Writer w = new FileWriter(new File("start.sh"));
+					w.write(commandLine.toString());
+					w.close();
+				}
+				JOptionPane.showMessageDialog(null, "已生成启动脚本");
+			} else {
+				OutputStreamWriter w = new OutputStreamWriter(new FileOutputStream(f), "utf-8");
+				w.write("# java main 类\n");
+				w.write("main.class=\n");
+				w.write("# classpath 路径 , 用逗号隔开的多个目录");
+				w.write("classpath=classes,config\n");
+				w.write("# lib 路径 , 用逗号隔开的多个目录");
+				w.write("libdir=lib\n");
+				w.close();
+				JOptionPane.showMessageDialog(null, "系统自动生成了starter.properties，請填写main.class后再次执行！");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "生成脚本出错:" + e.getMessage());
+		}
+	}
+
+	private static void appendCpDirs(Set<String> classpathes, Set<String> libDirs, StringBuffer commandLine, String spliter) {
+		for (String cls : classpathes) {
+			if (new File(cls).isDirectory()) {
+				commandLine.append("classes").append(spliter);
+			}
+		}
+		for (String dir : libDirs) {
+			File libDir = new File(dir);
+			if (libDir.isDirectory()) {
+				List<File> fileList = getLibFile(libDir);
+				for (File file : fileList) {
+					commandLine.append(
+							file.getAbsolutePath().substring(libDir.getAbsolutePath().length() - libDir.getName().length()))
+							.append(spliter);
+				}
+			}
+		}
+	}
+
+	private static List<File> getLibFile(File libDir) {
+		List<File> fileList = new ArrayList<File>();
+		File[] subFileList = libDir.listFiles();
+		for (int i = 0; i < subFileList.length; i++) {
+			File file = subFileList[i];
+			if (file.isFile()) {
+				if (file.getName().endsWith(".jar") || file.getName().endsWith(".zip")) {
+					fileList.add(file);
+				}
+			} else if (file.isDirectory()) {
+				fileList.addAll(getLibFile(file));
+			}
+		}
+		return fileList;
+	}
+}
